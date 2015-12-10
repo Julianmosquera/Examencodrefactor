f1dd151df9ce5b0e503481ec2ce67e51c24c0d71
julianlinux <julianlinux@julianlinux-X555LA>
2015-12-10 08:58:44 +0100

    Final del cambio de variables contador conta contador1  conta1

--- src/examenrefactorizar/Examenrefactorizar.java
+++ src/examenrefactorizar/Examenrefactorizar.java
@@ -18,13 +18,13 @@ public class Examenrefactorizar {
             for (int i = 1; i <= 99999; i++) {
                 int aux = i;
 
-                int contador = 0;
+                int conta = 0;
 
                 while (aux != 0) {
                     aux = aux / 10;
-                    contador++;
+                    conta++;
                 }
-                ndigitos = contador;
+                ndigitos = conta;
 
                 if (ndigitos == digitos) {
                     if (i < 4) {
@@ -33,7 +33,7 @@ public class Examenrefactorizar {
                         if (i % 2 == 0) {
                             p = false;
                         } else {
-                            int contador1 = 0;
+                            int conta1 = 0;
                             int i1 = 1;
                             int k = (i - 1) / 2;
                             if (k % 2 == 0) {
@@ -42,15 +42,15 @@ public class Examenrefactorizar {
 
                             while (i1 <= k) {
                                 if (i % i1 == 0) {
-                                    contador1++;
+                                    conta1++;
                                 }
                                 i1 += 2;
-                                if (contador1 == 2) {
+                                if (conta1 == 2) {
                                     i1 = k + 1;
                                 }
                             }
 
-                            if (contador1 == 1) {
+                            if (conta1 == 1) {
                                 p = true;
                             }
                         }
